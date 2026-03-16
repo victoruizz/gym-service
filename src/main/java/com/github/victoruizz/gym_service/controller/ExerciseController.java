cb294ce27633d3c9587d058137237536be3391a3
Victor Ruiz <ruizvictor077@outlook.com>
2026-03-16 20:22:34 -0300

    fix: adding GetMapping annotation to class getExercise

--- src/main/java/com/github/victoruizz/gym_service/controller/ExerciseController.java
+++ src/main/java/com/github/victoruizz/gym_service/controller/ExerciseController.java
@@ -2,12 +2,14 @@ package com.github.victoruizz.gym_service.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/exercise")
 public class ExerciseController {
     
-    
+    @GetMapping()
     public  String getExercises(){
         return "Push-ups\nSquats\nLunges\nPlank\nBurpees\nMountain Climbers\nJumping Jacks";
     }
